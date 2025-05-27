9a05e4ad3846a8966f00500920bd3a4ff3a650f6
Administrator <bekkapan@gmail.com>
2025-05-27 21:03:42 +0200

    2

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/style1.css"; // adatta il percorso in base a dove metti il file CSS
+
+function WelcomePage() {
+    return (
+        <>
+            <header>Benvenuto su Due!!</header>
+            <div className="center">
+                <h2>
+                    <Link to="/login" className="button">Play</Link>
+                </h2>
+            </div>
+        </>
+    );
+}
+
+export default WelcomePage;
